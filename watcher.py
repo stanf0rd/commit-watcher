a466d4c24e44da3218e53fd8acc5a803938d5e7c
Dmitry Paliy <dmitrydpaliy@gmail.com>
2019-10-12 01:38:01 +0300

    [CREATE] main flask app called watcher created; pylint init

--- watcher.py
+++ watcher.py
@@ -2,7 +2,7 @@
 """
 Commit-watcher entry point
 
-Recieves requests, check signature and proceeds data to modules
+Recieves requests, checks signature and proceeds data to modules
 
 Powered by Flask
 """
@@ -11,22 +11,29 @@ Powered by Flask
 import hashlib
 import hmac
 import logging
+from enum import Enum
 
 # Third party imports
 from flask import Flask, abort, request, jsonify
-from flask.logging import create_logger
+from flask.logging import default_handler
 
 # Local application imports
 from config import WATCHER_CONFIG
 
 
-APP = Flask(__name__)
-LOGGER = create_logger(APP)
+class CertificateError(Enum):
+    """ Certificate error descriptions """
+    INVALID = 1
+    NO_HEADER = 2
 
-logging.basicConfig(
-    format='%(asctime)s - %(message)s',
-    datefmt='%d-%b-%y %H:%M:%S',
-)
+
+APP = Flask(__name__)
+LOGGER = APP.logger
+
+default_handler.setFormatter(logging.Formatter(
+    fmt='%(asctime)s - %(message)s',
+    datefmt='%d/%b/%y %H:%M:%S'
+))
 
 
 @APP.route('/<app_id>', methods=['POST'])
@@ -35,27 +42,57 @@ def common(app_id):
     Checks route and signature
     Sends payload to modules
     """
-    if app_id in WATCHER_CONFIG.get('repos'):
-        if 'X-Hub-Signature' not in request.headers:
-            LOGGER.warning("Request with no signature: /%s", app_id)
-            return jsonify({"msg": "No signature."}), 403
 
-        counted_signature = create_signature(
-            WATCHER_CONFIG.get('repos').get(app_id).get('secret'),
-            request.data
-        )
-        signature_is_valid = check_signature(
-            counted_signature,
-            request.headers.get('X-Hub-Signature'),
-        )
-        if not signature_is_valid:
-            LOGGER.warning("Request with invalid signature: /%s", app_id)
-            return jsonify({"msg": "Invalid signature."}), 403
-
-        return '', 200
-    else:
+    if app_id not in WATCHER_CONFIG.get('repos'):
         LOGGER.info("Unknown request: /%s", app_id)
         abort(404)
+
+    app_config = WATCHER_CONFIG.get('repos').get(app_id)
+
+    check_result = check_signature(app_config)
+    if check_result == CertificateError.INVALID:
+        LOGGER.warning("Request with invalid signature: /%s", app_id)
+        return jsonify({"msg": "Invalid signature."}), 403
+    elif check_result == CertificateError.NO_HEADER:
+        LOGGER.warning("Request with no signature: /%s", app_id)
+        return jsonify({"msg": "No signature."}), 403
+    elif check_result != 0:
+        LOGGER.error("Unknown error while checking signature")
+        return jsonify({"msg": "Unknown signature error."}), 403
+
+    return "", 200
+
+
+def check_signature(config):
+    """ Checks signature validity reading current request """
+    if 'X-Hub-Signature' not in request.headers:
+        return CertificateError.NO_HEADER
+
+    signature_header = request.headers.get("X-Hub-Signature")
+
+    if APP.debug and signature_header == "DISABLE__SIGNATURE":
+        return 0
+
+    splitted = signature_header.split("sha1=", 1)
+
+    if len(splitted) == 1:
+        return CertificateError.INVALID
+
+    received_signature = splitted[1]
+
+    counted_signature = create_signature(
+        config.get('secret'),
+        request.data,
+    )
+    signature_is_valid = compare_signatures(
+        counted_signature,
+        received_signature,
+    )
+
+    if signature_is_valid:
+        return 0
+    else:
+        return CertificateError.INVALID
 
 
 def create_signature(secret, payload):
@@ -66,7 +103,7 @@ def create_signature(secret, payload):
     return signature
 
 
-def check_signature(first, second):
+def compare_signatures(first, second):
     """ Safely compares two signatures """
     return hmac.compare_digest(
         str(first),
